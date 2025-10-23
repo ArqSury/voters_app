2dd139b7e8dc6aaa42d4e20e8aa5b10a872241f3
ArqSury <Ariqsurya0510@gmail.com>
2025-10-23 14:12:10 +0700

    commit

--- lib/branches/about_app.dart
+++ lib/branches/about_app.dart
@@ -11,19 +11,28 @@ class _AboutAppState extends State<AboutApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.red),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(12),
+          padding: const EdgeInsets.all(8),
           child: SingleChildScrollView(
             child: Column(
               spacing: 8,
               children: [
                 Image(
-                  image: AssetImage("assets/images/logo/logoapp_final.png"),
-                  height: 240,
-                  width: 300,
+                  image: AssetImage('assets/images/logo/logoapp_final.png'),
+                  height: 200,
+                  width: 200,
                 ),
-                Text('"VOTERS ON!'),
+                Container(
+                  margin: const EdgeInsets.all(12),
+                  padding: const EdgeInsets.all(12),
+                  child: Text(
+                    '"VOTERS ON! adalah aplikasi yang berfungsi sebagai alat pengambilan sekaligus perhitungan suara dalam Pemilihan Umum secara online dengan harapan membuat Pemilihan Umum menjadi lebih effesien dan efektif dan masih berjunjung dengan asas LUBERJURDIL"',
+                    style: TextStyle(fontStyle: FontStyle.italic, fontSize: 16),
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
               ],
             ),
           ),
