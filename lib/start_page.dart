5828743824ee27d01c7913dd185bbf59216f1e11
ArqSury <Ariqsurya0510@gmail.com>
2025-10-23 10:38:02 +0700

    commit

--- lib/start_page.dart
+++ lib/start_page.dart
@@ -11,28 +11,30 @@ class _StartPageState extends State<StartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Colors.red, Colors.white],
-                    begin: AlignmentDirectional.topCenter,
-                    end: AlignmentDirectional.bottomCenter,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Colors.red, Colors.white],
+                      begin: AlignmentDirectional.topCenter,
+                      end: AlignmentDirectional.bottomCenter,
+                    ),
                   ),
-                ),
-                child: Image(
-                  image: AssetImage(
-                    "assets/images/logo/logo_voterson_nobg2.png",
+                  child: Image(
+                    image: AssetImage(
+                      "assets/images/logo/logo_voterson_nobg2.png",
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
