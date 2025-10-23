36271a2275433cf80c311c4e5c4a2e92e0105cda
ArqSury <Ariqsurya0510@gmail.com>
2025-10-23 10:37:20 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/voters_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.voters_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/start_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({super.key});
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.red, Colors.white],
+                    begin: AlignmentDirectional.topCenter,
+                    end: AlignmentDirectional.bottomCenter,
+                  ),
+                ),
+                child: Image(
+                  image: AssetImage(
+                    "assets/images/logo/logo_voterson_nobg2.png",
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
