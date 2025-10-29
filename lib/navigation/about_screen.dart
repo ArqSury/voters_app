78e883b383e68a48c24682d7d0ecf00eca4324a9
ArqSury <Ariqsurya0510@gmail.com>
2025-10-29 14:33:11 +0700

    commit

---
+++ lib/navigation/about_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AboutScreen extends StatefulWidget {
+  const AboutScreen({super.key});
+
+  @override
+  State<AboutScreen> createState() => _AboutScreenState();
+}
+
+class _AboutScreenState extends State<AboutScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(child: Column(children: [Text('About')])),
+      ),
+    );
+  }
+}
--- lib/views/main_page.dart
+++ lib/views/main_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:voters_app/constant/app_color.dart';
+import 'package:voters_app/navigation/about_screen.dart';
+import 'package:voters_app/navigation/home_screen.dart';
+import 'package:voters_app/navigation/result_screen.dart';
+import 'package:voters_app/navigation/user_profile.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -9,8 +14,44 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int _selectedBar = 0;
+
+  static const List<Widget> _navOptions = [
+    HomeScreen(),
+    UserProfile(),
+    ResultScreen(),
+    AboutScreen(),
+  ];
+  void _onBarTapped(int bar) {
+    setState(() {
+      _selectedBar = bar;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold());
+    return SafeArea(
+      child: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Rumah'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profil'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bar_chart),
+              label: 'Hasil',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.question_mark),
+              label: 'Tentang',
+            ),
+          ],
+          currentIndex: _selectedBar,
+          selectedItemColor: AppColor.primary,
+          backgroundColor: AppColor.button,
+          onTap: _onBarTapped,
+        ),
+        body: Center(child: _navOptions.elementAt(_selectedBar)),
+      ),
+    );
   }
 }
