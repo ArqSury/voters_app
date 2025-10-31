7cd4a2733ac60101c84f1d797b03ed09975809f7
ArqSury <Ariqsurya0510@gmail.com>
2025-10-31 14:42:18 +0700

    commit

--- lib/navigation/about_screen.dart
+++ lib/navigation/about_screen.dart
@@ -10,9 +10,39 @@ class AboutScreen extends StatefulWidget {
 class _AboutScreenState extends State<AboutScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Center(child: Column(children: [Text('About')])),
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 12,
+                children: [
+                  Image(
+                    image: AssetImage('assets/images/logo/logoapp_final.png'),
+                    height: 300,
+                  ),
+                  Text(
+                    '"Voters On! adalah aplikasi pemilu untuk mengambil dan menghitung suara pilihan rakyat Indonesia yang menjunjung asas LUBERJURDIL. Aplikasi ini memiliki beberapa fitur seperti profil biodata calon-calon anggota pemerintahan yang akan dipilih. Selain itu juga aplikasi ini memiliki fitur Live Count Vote dimana akan ada perhitungan suara secara langsung dan seksama yang bisa dilihat oleh seluruh rakyat Indonesia sehingga diharapkan pemilu berjalan adil serta efektif dan efesien"',
+                    style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
+                    textAlign: TextAlign.justify,
+                  ),
+                  SizedBox(height: 60),
+                  Text(
+                    'Creator',
+                    style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+                  ),
+                  Image(
+                    image: AssetImage('assets/images/logo/smile.png'),
+                    height: 200,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/navigation/candidate_screen.dart
+++ lib/navigation/candidate_screen.dart
@@ -16,52 +16,50 @@ class _CandidateScreenState extends State<CandidateScreen> {
         appBar: AppBar(backgroundColor: AppColor.primary),
         body: Stack(
           alignment: AlignmentDirectional.bottomCenter,
+          children: [buildBackground(), buildLayer()],
+        ),
+      ),
+    );
+  }
+
+  Center buildLayer() {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
           children: [
-            buildBackground(),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(12),
-                child: Column(
-                  children: [
-                    Text(
-                      'Pilih Pasangan',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
+            Text(
+              'Pilih Pasangan',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Divider(color: Colors.black, indent: 20, endIndent: 20),
+            SizedBox(height: 10),
+            DefaultTabController(
+              length: 3,
+              child: Column(
+                children: [
+                  const TabBar(
+                    tabs: [
+                      Tab(text: 'Pasangan 1'),
+                      Tab(text: 'Pasangan 2'),
+                      Tab(text: 'Pasangan 3'),
+                    ],
+                  ),
+                  Container(
+                    height: 500,
+                    width: double.infinity,
+                    margin: const EdgeInsets.all(20),
+                    padding: const EdgeInsets.all(8),
+                    decoration: decorationContainer(),
+                    child: TabBarView(
+                      children: [
+                        Center(child: Text('Pasangan 1')),
+                        Center(child: Text('Pasangan 2')),
+                        Center(child: Text('Pasangan 3')),
+                      ],
                     ),
-                    Divider(color: Colors.black, indent: 20, endIndent: 20),
-                    SizedBox(height: 10),
-                    DefaultTabController(
-                      length: 3,
-                      child: Column(
-                        children: [
-                          const TabBar(
-                            tabs: [
-                              Tab(text: 'Pasangan 1'),
-                              Tab(text: 'Pasangan 2'),
-                              Tab(text: 'Pasangan 3'),
-                            ],
-                          ),
-                          Container(
-                            height: 500,
-                            width: double.infinity,
-                            margin: const EdgeInsets.all(20),
-                            padding: const EdgeInsets.all(8),
-                            decoration: decorationContainer(),
-                            child: TabBarView(
-                              children: [
-                                Center(child: Text('Pasangan 1')),
-                                Center(child: Text('Pasangan 2')),
-                                Center(child: Text('Pasangan 3')),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ],
