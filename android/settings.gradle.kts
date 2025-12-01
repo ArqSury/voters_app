fb7da1431eeee6b3d0ef82a4622c2d23078da499
ArqSury <Ariqsurya0510@gmail.com>
2025-12-01 10:49:00 +0700

    commit

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -18,7 +18,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.1.6"
+    id("dev.flutter.flutter-plugin-loader") version "1.1.8"
     id("com.android.application") version "8.9.1" apply false
     // START: FlutterFire Configuration
     id("com.google.gms.google-services") version("4.3.15") apply false
